06201aa62836c0e4aa20ef903b7c3c46bebaf03f
Gabriel Freitas Souza <gabrielsouza45@live.com>
2023-06-27 13:48:59 -0300

    Create ErroDeConversaoDeAnoException.java

---
+++ ScreenMatch/src/br/com/alura/screenmatch/excecao/ErroDeConversaoDeAnoException.java
@@ -0,0 +1,12 @@
+package br.com.alura.screenmatch.excecao;
+
+public class ErroDeConversaoDeAnoException extends RuntimeException {
+    private String mensagem;
+    public ErroDeConversaoDeAnoException(String mensagem) {
+        this.mensagem = mensagem;
+    }
+
+    public String getMensagem() {
+        return this.mensagem;
+    }
+}
