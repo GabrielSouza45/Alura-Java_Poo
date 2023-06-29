71a35bf2abc19f8c8821c781c8093a49a0781c7f
Gabriel Freitas Souza <gabrielsouza45@live.com>
2023-06-29 15:52:11 -0300

    Finalização ScreenMatch

--- ScreenMatch/src/br/com/alura/screenmatch/model/TituloOMDb.java
+++ ScreenMatch/src/br/com/alura/screenmatch/model/TituloOMDb.java
@@ -1,4 +1,5 @@
 package br.com.alura.screenmatch.model;
 
+//Classe Abstrata Record : cria automaticamente construtores e atributos do tipo FINAL que estão dentro do parâmetro
 public record TituloOMDb(String title, String year, String runtime) {
 }
