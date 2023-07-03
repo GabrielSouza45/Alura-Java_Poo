76f313b11f6a59311164d0d524d8a03dbf5e6e91
Gabriel Freitas Souza <gabrielsouza45@live.com>
2023-07-03 15:16:54 -0300

    Retorno da lista de endereços

--- Desafio - CEP/src/br/com/alura/Main/BuscaPorRua.java
+++ Desafio - CEP/src/br/com/alura/Main/BuscaPorRua.java
@@ -1,6 +1,7 @@
 package br.com.alura.Main;
 
 import br.com.alura.Model.Endereco;
+import br.com.alura.Model.EnderecoArray;
 import com.google.gson.Gson;
 
 import java.io.IOException;
@@ -9,46 +10,61 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.text.Normalizer;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class BuscaPorRua {
-    public Endereco buscaCep() throws IOException, InterruptedException {
+    private String cidade;
+    private String estado;
+    private String rua;
+    private String formato = "json";
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        BuscaPorRua buscaPorRua = new BuscaPorRua();
+        buscaPorRua.buscaCep();
+    }
+
+    public EnderecoArray[] buscaCep() throws IOException, InterruptedException {
 
         Scanner sc = new Scanner(System.in);
         System.out.println("Digite sua cidade: ");
-        String cidade = sc.nextLine();
+        this.cidade = sc.nextLine();
 
         System.out.println("Digite sua Rua:");
-        String rua = sc.nextLine();
+        this.rua = sc.nextLine();
 
         System.out.println("Digite seu Estado: (2 letras)");
-        String estado = sc.nextLine();
+        this.estado = sc.nextLine();
 
-        String formato = "json";
-
-        cidade = Normalizer.normalize(cidade, Normalizer.Form.NFD).replaceAll("[^\\p{ASCII}]", "").replaceAll(" ", "%20");
-        rua = Normalizer.normalize(rua, Normalizer.Form.NFD).replaceAll("[^\\p{ASCII}]", "").replaceAll(" ", "%20");
-        estado = Normalizer.normalize(estado, Normalizer.Form.NFD).replaceAll("[^\\p{ASCII}]", "").replaceAll(" ", "%20");
+        this.cidade = Normalizer.normalize(cidade, Normalizer.Form.NFD).replaceAll("[^\\p{ASCII}]", "").replaceAll(" ", "%20");
+        this.rua = Normalizer.normalize(rua, Normalizer.Form.NFD).replaceAll("[^\\p{ASCII}]", "").replaceAll(" ", "%20");
+        this.estado = Normalizer.normalize(estado, Normalizer.Form.NFD).replaceAll("[^\\p{ASCII}]", "").replaceAll(" ", "%20");
 
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(String.format(
                         "https://viacep.com.br/ws/%s/%s/%s/%s/",
-                        estado,
-                        cidade,
-                        rua,
-                        formato)))
+                        this.estado,
+                        this.cidade,
+                        this.rua,
+                        this.formato)))
                 .build();
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
 
-        String json = response.body();
-
-        System.out.println(json);
 
         Gson gson = new Gson();
-        Endereco endereco = gson.fromJson(json, Endereco.class);
+        String json = response.body();
+//        System.out.println(json);
 
-        return endereco;
+        EnderecoArray[] enderecos = gson.fromJson(json, EnderecoArray[].class);
+//        if (enderecos.length >= 1){
+//            System.out.println(enderecos[0]);
+//        }
+//        for (EnderecoArray end : enderecos){
+//            System.out.println(end.toString());
+//        }
+
+        return enderecos;
     }
 }
