ade479b4d55c72c2d53ddbbaebb8ee9164e4f745
Gabriel Freitas Souza <gabrielsouza45@live.com>
2023-07-03 15:16:11 -0300

    Criada classe EnderecoArray para receber a lista de endereços
    Para o caso da pessoa pesquisar pela rua e ciadade estado ao inves de usar o cep

---
+++ Desafio - CEP/src/br/com/alura/Model/EnderecoArray.java
@@ -0,0 +1,16 @@
+package br.com.alura.Model;
+
+import java.util.ArrayList;
+
+public class EnderecoArray extends Endereco{
+    ArrayList<Endereco> enderecos;
+
+    public EnderecoArray(){
+        this.enderecos = new ArrayList<>();
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
